5a84e1cdf10cc8dec533a3802e86e8f520ef6e5f
Brandon Le <brandon.le@wsu.edu>
2019-11-20 11:20:37 -0800

    basic auto replace plugin

--- editor/src/App.js
+++ editor/src/App.js
@@ -1,26 +1,59 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import { Editor } from "slate-react";
+import { Value } from "slate";
+import AutoReplace from "./plugin/AutoComplete.js";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const initialValue = Value.fromJSON({
+  document: {
+    nodes: [
+      {
+        object: "block",
+        type: "paragraph",
+        nodes: [
+          {
+            object: "text",
+            text: "Enter equation."
+          }
+        ]
+      }
+    ]
+  }
+});
+
+const replace = AutoReplace({
+  trigger: "space",
+  before: /(\\frac)/,
+  change: (change, e, matches) => {
+    return change.setBlocks({ type: "quote" });
+  }
+});
+
+const plugins = [replace];
+
+// Define our app...
+class App extends React.Component {
+  // Set the initial value when the app is first constructed.
+  state = {
+    value: initialValue
+  };
+
+  // On change, update the app's React state with the new editor value.
+  onChange = ({ value }) => {
+    this.setState({ value });
+  };
+
+  // Render the editor.
+  render() {
+    return (
+      <Editor
+        plugins={plugins}
+        value={this.state.value}
+        onChange={this.onChange}
+      />
+    );
+  }
 }
 
 export default App;
